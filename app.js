be2c9fcf0dd0bbc102bdf93c32c402a42e3fb590
staq001 <dame.hush@gmail.com>
2024-07-01 11:56:13 +0100

    fixed location and weather

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const location = require("./location.js")
+const { location } = require("./location.js")
+const { weatherFunc } = require("./location.js")
 
 const app = express();
 
@@ -9,19 +10,26 @@ app.set('trust-proxy', true);
 
 app.use(express.json())
 
+app.get('/', (req, res) => {
+  res.send(`
+    <p>Navigate your way <a href=https://${req.hostname}/api/hello?name=Insert_name target="_blank">here</a> to get a greeting!</p>
+    `);
+})
+
 app.get('/api/hello', async (req, res) => {
-  const match = {};
-  const client_ip = req.headers['cf-connecting-ip'] || req.headers['x-real-ip'] || req.headers['x-forwarded-for'] || req.socket.remoteAddress
+  try {
+    const { visitor_name } = req.query;
+    const client_ip = req.headers['cf-connecting-ip'] || req.headers['x-real-ip'] || req.headers['x-forwarded-for'] || req.socket.remoteAddress
+    const locale = location(client_ip)
 
-  if (req.query.visitor_name) {
-    match.visitor_name = req.query.visitor_name;
-  };
-
-  res.send({
-    client_ip,
-    location: location(client_ip),
-    greeting: `Hello, ${match.visitor_name}!`
-  })
+    res.send({
+      client_ip,
+      location: location('102.88.36.60'),
+      greeting: `Hello, ${visitor_name}!, the temperature is ${await weatherFunc(locale)} in ${locale}`
+    })
+  } catch (err) {
+    res.send({ status: "failed", error_msg: err.message });
+  }
 })
 
 
--- location.js
+++ location.js
@@ -1,4 +1,5 @@
 const geoip = require('geoip-lite');
+const { OpenWeatherAPI } = require("openweather-api-node")
 
 function location(ip) {
   const geo = geoip.lookup(ip)
@@ -6,4 +7,25 @@ function location(ip) {
   return geo.city;
 }
 
-module.exports = location;
+
+
+async function weatherFunc(location) {
+  let temp;
+  let weather = new OpenWeatherAPI({
+    key: 'f6dba2e71df78d9729c40da3b5350d01',
+    locationName: location,
+    units: "imperial"
+  })
+
+  const current = await weather.getCurrent().then(async data => {
+    temp = data.weather.temp;
+    tempInCelsius = Math.round((temp.cur - 32) * (5 / 9) * 100) / 100;
+    // console.log(temp.cur);
+    return tempInCelsius;
+  })
+  return current;
+}
+
+
+
+module.exports = { location, weatherFunc };
